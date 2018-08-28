bbrezillon/apitrace

commit 84398075208f7495b6d09fda7c8bb69b894c81ed

Add GlFunctions::GetGlExtensions() helper
Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

retrace/daemon/gldispatch/glframe_glhelper.cpp
@@ -704,6 +704,28 @@ GlFunctions::GetStringi(GLenum pname, GLuint index) {
   return ((GETSTRINGI)pGetStringi)(pname, index);
 }
 
+void GlFunctions::GetGlExtensions(std::string &extensions)
+{
+	const GLubyte *ext;
+	GLint count = 0;
+
+	extensions.clear();
+
+	ext = GlFunctions::GetString(GL_NUM_EXTENSIONS);
+	if (ext) {
+		extensions = (const char *)ext;
+		return;
+	}
+
+	GlFunctions::GetIntegerv(GL_NUM_EXTENSIONS, &count);
+
+	for (int i = 0; i < count; i++) {
+		ext = GlFunctions::GetStringi(GL_EXTENSIONS, i);
+		extensions += " ";
+		extensions += (const char *)ext;
+	}
+}
+
 void GlFunctions::GetPerfQueryInfoINTEL(GLuint queryId, GLuint queryNameLength,
                                         GLchar *queryName, GLuint *dataSize,
                                         GLuint *noCounters, GLuint *noInstances,

retrace/daemon/gldispatch/glframe_glhelper.hpp
@@ -88,6 +88,7 @@ class GlFunctions {
   static void GetIntegerv(GLenum pname, GLint *params);
   static const GLubyte *GetString(GLenum pname);
   static const GLubyte *GetStringi(GLenum pname, GLuint index);
+  static void GetGlExtensions(std::string &extensions);
   static void GetPerfQueryInfoINTEL(GLuint queryId, GLuint queryNameLength,
                                     GLchar *queryName, GLuint *dataSize,
                                     GLuint *noCounters, GLuint *noInstances,

commit af96657db8e5c073bafad6f4841be491b49214f0

Fix GlFunctions::GetGlExtensions()
Retrieve GL_EXTENSIONS string instead of trying to get
GL_NUM_EXTENSIONS as a string.

Fixes: 84398075208f ("Add GlFunctions::GetGlExtensions() helper")
Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>

retrace/daemon/gldispatch/glframe_glhelper.cpp
@@ -713,7 +713,7 @@ GlFunctions::GetGlExtensions(std::string *extensions) {
 
   extensions->clear();
 
-  ext = GlFunctions::GetString(GL_NUM_EXTENSIONS);
+  ext = GlFunctions::GetString(GL_EXTENSIONS);
   if (ext) {
     *extensions = (const char *)ext;
     return;